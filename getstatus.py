e3b3e8580e2ca23e5e5d21cb478eaf51e8fc48b1
Casey Law <caseyjlaw@gmail.com>
2022-02-18 10:47:31 -0800

    using requests to check url

--- getstatus.py
+++ getstatus.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 import time
 import os.path
+import requests
 
 
 projects = ['ovrolwa', 'dsa110']
 mdpath = './status/'
 
 # map group to service and service to server:port
-services = {'ovrolwa': {'webUI': '?', 'grafana': 'grafanaservice.lwa.pvt:3000', 'dashboard': 'lxdlwacr.sas.pvt:500?'},
+services = {'ovrolwa': {'webUI': None, 'grafana': 'grafanaservice.lwa.pvt:3000', 'dashboard': 'lxdlwacr.sas.pvt:500?'},
             'dsa110': {'webUI': 'webserverUIservice.sas.pvt:9090', 'grafana': 'grafanaservice.ovro.pvt:3000',
-            'dashboard': 'lxd110h23.sas.pvt:5008', 'hiplot': 'dsa-storage.ovro.pvt:5007', 'bokeh': 'bokehservice.ovro.pvt:5006'}}
+                       'dashboard': 'lxd110h23.sas.pvt:5008', 'hiplot': 'dsa-storage.ovro.pvt:5007',
+                       'bokeh': 'bokehservice.ovro.pvt:5006', 'archive': 'code.deepsynoptic.org/dsa110-archive'}}
 
 for project in projects:
     with open(os.path.join(mdpath, f'{project}.md'), 'w') as fp:
         for service in services[project].keys():
-            fp.write(f'{project} | {service} | Down | {time.ctime()}\n')
+            url = f'http://{services[project][service]}'
+            print(url)
+            if services[project][service] is not None:
+                status = 'Down'
+                try:
+                    resp = requests.get(url)
+                    if resp.ok:
+                        status = 'Up'
+                except requests.exceptions.ConnectionError:
+                    status = 'Down'
+            else:
+                status = 'in development'
+            fp.write(f'{project} | {service} | {status} | {time.ctime()}\n')
 
 # Projects are groups of servicese (e.g., OVRO-LWA, DSA-110)
 # Services are individual web apps (e.g., webUI, hiplot)
--- status/ovrolwa.md
+++ status/ovrolwa.md
@@ -1,3 +1,3 @@
-ovrolwa | webUI | Down | Fri Feb 18 10:33:48 2022
-ovrolwa | grafana | Down | Fri Feb 18 10:33:48 2022
-ovrolwa | dashboard | Down | Fri Feb 18 10:33:48 2022
+ovrolwa | webUI | in development | Fri Feb 18 10:47:05 2022
+ovrolwa | grafana | Down | Fri Feb 18 10:47:05 2022
+ovrolwa | dashboard | Down | Fri Feb 18 10:47:05 2022
