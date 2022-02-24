b70295bdd7e48a94b8e899556b8e0afd6173e56a
Casey Law <caseyjlaw@gmail.com>
2022-02-24 11:22:37 -0800

    adding lwasnap

--- getstatus.py
+++ getstatus.py
@@ -8,7 +8,8 @@ projects = ['ovrolwa', 'dsa110']
 mdpath = './status/'
 
 # map group to service and service to server:port
-services = {'ovrolwa': {'webUI': None, 'grafana': 'grafanaservice.lwa.pvt:3000', 'dashboard': 'lxdlwacr.sas.pvt:500?'},
+services = {'ovrolwa': {'webUI': None, 'grafana': 'grafanaservice.lwa.pvt:3000', 'dashboard': 'lxdlwacr.sas.pvt:500?',
+                        'LWASNAP': 'greghell.github.io/LWASNAP'},
             'dsa110': {'webUI': 'webserverUIservice.sas.pvt:9090', 'grafana': 'grafanaservice.ovro.pvt:3000',
                        'dashboard': 'lxd110h23.sas.pvt:5008', 'hiplot': 'dsa-storage.ovro.pvt:5007',
                        'bokeh': 'bokehservice.sas.pvt:5006', 'archive': 'code.deepsynoptic.org/dsa110-archive'}}
